5d8bfa2fe3903e8cee9b35df511b62f33cd0d0de
alfer91 <dasungwkd@naver.com>
2020-07-11 21:32:12 +0900

    @Around 와 ProceedingJoinPoint

--- src/main/java/org/zerock/aop/LogAdvice.java
+++ src/main/java/org/zerock/aop/LogAdvice.java
@@ -1,6 +1,10 @@
 package org.zerock.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -31,5 +35,30 @@ public class LogAdvice {
 		
 		log.info("exception....!!!!");
 		log.info("exception: " + exception);
+	}
+	
+	/* Arround는 직접 메서드를 실행할 수 있는 권한이 있어 실행 전, 후 처리 가능 (ProceedingJoinPoint로 구체적 처리 가능) */
+	@Around("execution(* org.zerock.service.SampleService*.*(..))")
+	public Object logTime(ProceedingJoinPoint pjp) {
+		
+		long start = System.currentTimeMillis();
+		
+		log.info("Target: " + pjp.getTarget());
+		log.info("Param: " + Arrays.toString(pjp.getArgs()));
+		
+		//invoke method
+		Object result = null;
+		
+		try {
+			result = pjp.proceed();   // 메서드 실행
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		log.info("TIME: " + (end - start));   // 메서드 실행 시간
+		
+		return result;   // @Around가 적용 되는 메서드는 리턴타입이 메서드 실행 결과를 반환하는 형태여야 함 (void 안됨)
 	}
 }
